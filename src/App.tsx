6a10f131ef5b7a33cadfb64cce651db1ea6a0a82
Ali <abenrami06@gmail.com>
2025-04-19 22:45:14 -0500

    Refactor authentication component and update package dependencies
    - Moved AuthScreen component to a new directory structure under Pages.
    - Updated App component to import AuthScreen from the new location.
    - Removed old AuthScreen implementation and replaced it with the new one.
    - Cleaned up package.json by removing duplicate dependencies and updating Vite version.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import SplashPage from './Pages/SplashPage'
-import LoadingPage from './Pages/LoadingPage'
-import ReportPage from './Pages/ReportPage'
-import AuthScreen from './AuthScreen'
-import AboutUsPage from './Pages/AboutUsPage'
-import MainLayout from './Layouts/MainLayout'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SplashPage from "./Pages/SplashPage";
+import LoadingPage from "./Pages/LoadingPage";
+import ReportPage from "./Pages/ReportPage";
+import AuthScreen from "./Pages/AuthScreen";
+import AboutUsPage from "./Pages/AboutUsPage";
+import MainLayout from "./Layouts/MainLayout";
 // Add TechnicalPage and AboutUsPage if you have them
 
 function App() {
-
   return (
-    <BrowserRouter>
+    <BrowserRouter
+      future={{ v7_startTransition: true, v7_relativeSplatPath: true }}
+    >
       <Routes>
         <Route element={<MainLayout />}>
           <Route path="/" element={<SplashPage />} />
@@ -23,8 +24,7 @@ function App() {
         <Route path="/about" element={<AboutUsPage />} /> */}
       </Routes>
     </BrowserRouter>
-  )
-
+  );
 }
 
-export default App
+export default App;
